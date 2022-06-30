eb4bed5438e5725574e11b5f1de3a73638f5bad8
Harinand2 <107545342+Harinand2@users.noreply.github.com>
2022-06-30 08:19:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akbotmakersnew/LUCIFER.git /LUCIFER
+  git clone https://github.com/Harinand2/Michael.git /Michael
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LUCIFER
